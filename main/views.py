a236ad2d17392d786400c1b17f9fac3dfdc3851d
Vadym Drok <drokvadim@gmail.com>
2021-08-06 10:42:05 +0300

    w-1-1

--- main/views.py
+++ main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'main/index.html', {})
+# def index(request, room_name):
+#     return render(request, 'main/index.html', {})
 
-def room(request, room_name):
-    return render(request, 'main/room.html', {
-        'room_name': room_name
-    })
+def index(request):
+    return render(request, 'main/room.html', {})
+
+# def room(request, room_name):
+#     return render(request, 'main/room.html', {
+#         'room_name': room_name
+#     })
