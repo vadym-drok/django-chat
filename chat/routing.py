c3589351d4eec5df5a6ba0b613ab54abb50cea70
Vadym Drok <drokvadim@gmail.com>
2021-08-09 21:21:13 +0300

    heroku1

--- chat/routing.py
+++ chat/routing.py
@@ -2,7 +2,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import main.routing
 
-ASGI_APPLICATION = "chat.routing.application"
+
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
